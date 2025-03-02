a2c39896e6a172259bfca6703399479324c0f92e
Ricky John Garciano Reusora <175661621+akirachoi01@users.noreply.github.com>
2025-03-02 19:42:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on https://chats-production-f0f5.up.railway.app/:${PORT}`);
 });
